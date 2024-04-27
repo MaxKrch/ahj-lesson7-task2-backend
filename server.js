d37a80582c369f939f93801bb8ddb01b962b87eb
Max Kyrchanov <max.kyrchanov@gmail.com>
2024-04-28 00:27:12 +0500

    v1

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const Koa = require('koa');
 const { koaBody } = require('koa-body');
 const koaStatic = require('koa-static'); //возможно, перенести
 const cors = require('@koa/cors');
-
 const path = require('path'); 
+const fs = require('fs');;
+
+const { getExt, saveToDisc, removeFromDisc } = require('./api/images.js');
+const { getListImages, addImgToList,	upgImgToList,	delImgFromList } = require('./api/database.js');
 
 const app = new Koa();
 const public = path.join(__dirname, '/public');
 
-const { processingRequest } = require('./src/api/main.js');
+
 
 app.use(koaBody({
 	text: true,
@@ -31,6 +34,108 @@ app.use(koaBody({
 			ctx.response.body = "Сайт временно недоступен"
 		}
 	})
+
+
+const processingRequest = async (query, body, files = null) => {
+	const res = {
+		success: false,
+		data: null, 
+	}
+
+	const queryArray = query.split('&');
+	const actString = queryArray[0];
+	const actArray = actString.split('=');
+	const act = actArray[1];
+
+	try{
+		switch(act) {
+			case 'getAllImg':
+				res.data = await getImages();
+				if(res.data) {
+					res.success = true;
+				}
+				break;
+
+			case 'saveImg':
+				res.data = await saveImg(body.name, files.file);
+				if(res.data) {
+					res.success = true;
+				}
+				break;
+
+			case 'upgImg':
+				res.data = await upgImg(body.id, body.newName);
+				if(res.data) {
+					res.success = true;
+				}
+				break;
+
+			case 'delImg':
+				res.data = await delImg(body.id);
+				if(res.data) {
+					res.success = true;
+				}
+				break;
+
+			case 'sortImg':
+				console.log('sor');
+				break;
+
+			default:
+				res.success = true;
+				res.body = "Ошибка в запросе"
+		}
+		
+	} catch (err) {
+		res.body = "База данных сейчас недоступна"
+	}
+	return res;
+}
+
+const showError = (error) => {
+	console.log(`Что-то пошло не так: ${error}`)
+}
+
+const saveImg = async (name, img) => {
+	const newImg = await saveToDisc(img);
+	newImg.name = name;
+
+	if(!await addImgToList(newImg)) {
+		return false;
+	}
+
+	return newImg;
+}
+
+const upgImg = async (id, name) => {
+	const updatedImg = await upgImgToList(id, name)
+	
+	if(!updatedImg) {
+		return false;
+	}
+
+	return updatedImg;
+}
+
+const delImg = async (id) => {
+	const link = await delImgFromList(id);
+	
+	if (!await removeFromDisc(link)) {
+		return false;
+	}
+
+	return id;
+}
+
+const getImages = async() => {
+	const fullImages = await getListImages()
+	
+	if(!fullImages) {
+		return false;
+	}
+
+	return fullImages;
+}
 
 const port = process.env.PORT || 7070;
 const server = http.createServer(app.callback()).listen(port)
--- src/api/main.js
+++ src/api/main.js
@@ -1,108 +1,108 @@
-const fs = require('fs');;
+// const fs = require('fs');;
 
-const { getExt, saveToDisc, removeFromDisc } = require('./images.js');
-const { getListImages, addImgToList,	upgImgToList,	delImgFromList } = require('./database.js');
+// const { getExt, saveToDisc, removeFromDisc } = require('./images.js');
+// const { getListImages, addImgToList,	upgImgToList,	delImgFromList } = require('./database.js');
 
-const processingRequest = async (query, body, files = null) => {
-	const res = {
-		success: false,
-		data: null, 
-	}
+// const processingRequest = async (query, body, files = null) => {
+// 	const res = {
+// 		success: false,
+// 		data: null, 
+// 	}
 
-	const queryArray = query.split('&');
-	const actString = queryArray[0];
-	const actArray = actString.split('=');
-	const act = actArray[1];
+// 	const queryArray = query.split('&');
+// 	const actString = queryArray[0];
+// 	const actArray = actString.split('=');
+// 	const act = actArray[1];
 
-	try{
-		switch(act) {
-			case 'getAllImg':
-				res.data = await getImages();
-				if(res.data) {
-					res.success = true;
-				}
-				break;
+// 	try{
+// 		switch(act) {
+// 			case 'getAllImg':
+// 				res.data = await getImages();
+// 				if(res.data) {
+// 					res.success = true;
+// 				}
+// 				break;
 
-			case 'saveImg':
-				res.data = await saveImg(body.name, files.file);
-				if(res.data) {
-					res.success = true;
-				}
-				break;
+// 			case 'saveImg':
+// 				res.data = await saveImg(body.name, files.file);
+// 				if(res.data) {
+// 					res.success = true;
+// 				}
+// 				break;
 
-			case 'upgImg':
-				res.data = await upgImg(body.id, body.newName);
-				if(res.data) {
-					res.success = true;
-				}
-				break;
+// 			case 'upgImg':
+// 				res.data = await upgImg(body.id, body.newName);
+// 				if(res.data) {
+// 					res.success = true;
+// 				}
+// 				break;
 
-			case 'delImg':
-				res.data = await delImg(body.id);
-				if(res.data) {
-					res.success = true;
-				}
-				break;
+// 			case 'delImg':
+// 				res.data = await delImg(body.id);
+// 				if(res.data) {
+// 					res.success = true;
+// 				}
+// 				break;
 
-			case 'sortImg':
-				console.log('sor');
-				break;
+// 			case 'sortImg':
+// 				console.log('sor');
+// 				break;
 
-			default:
-				res.success = true;
-				res.body = "Ошибка в запросе"
-		}
+// 			default:
+// 				res.success = true;
+// 				res.body = "Ошибка в запросе"
+// 		}
 		
-	} catch (err) {
-		res.body = "База данных сейчас недоступна"
-	}
-	return res;
-}
+// 	} catch (err) {
+// 		res.body = "База данных сейчас недоступна"
+// 	}
+// 	return res;
+// }
 
-const showError = (error) => {
-	console.log(`Что-то пошло не так: ${error}`)
-}
+// const showError = (error) => {
+// 	console.log(`Что-то пошло не так: ${error}`)
+// }
 
-const saveImg = async (name, img) => {
-	const newImg = await saveToDisc(img);
-	newImg.name = name;
+// const saveImg = async (name, img) => {
+// 	const newImg = await saveToDisc(img);
+// 	newImg.name = name;
 
-	if(!await addImgToList(newImg)) {
-		return false;
-	}
+// 	if(!await addImgToList(newImg)) {
+// 		return false;
+// 	}
 
-	return newImg;
-}
+// 	return newImg;
+// }
 
-const upgImg = async (id, name) => {
-	const updatedImg = await upgImgToList(id, name)
+// const upgImg = async (id, name) => {
+// 	const updatedImg = await upgImgToList(id, name)
 	
-	if(!updatedImg) {
-		return false;
-	}
+// 	if(!updatedImg) {
+// 		return false;
+// 	}
 
-	return updatedImg;
-}
+// 	return updatedImg;
+// }
 
-const delImg = async (id) => {
-	const link = await delImgFromList(id);
+// const delImg = async (id) => {
+// 	const link = await delImgFromList(id);
 	
-	if (!await removeFromDisc(link)) {
-		return false;
-	}
+// 	if (!await removeFromDisc(link)) {
+// 		return false;
+// 	}
 
-	return id;
-}
+// 	return id;
+// }
 
-const getImages = async() => {
-	const fullImages = await getListImages()
+// const getImages = async() => {
+// 	const fullImages = await getListImages()
 	
-	if(!fullImages) {
-		return false;
-	}
+// 	if(!fullImages) {
+// 		return false;
+// 	}
 
-	return fullImages;
-}
+// 	return fullImages;
+// }
 
 
-module.exports = { processingRequest }
+// module.exports = { processingRequest }
